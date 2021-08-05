74a05f5dc07632cb03450712bd4a3dd90ada20fb
오수현 <44641501+dhtngus4921@users.noreply.github.com>
2021-08-05 16:38:05 +0900

    회원 관리 기능
    > 로그인 세부 기능 (자동 로그인, 아이디/비번 저장)
    > 회원 가입 세부 기능 (아이디/비번 중복 확인, 이메일 본인 인증, null 체크)
    > 아이디/비번 찾기 기능

--- app/src/main/java/com/example/bepro/LoginActivity.java
+++ app/src/main/java/com/example/bepro/LoginActivity.java
@@ -1,46 +1,124 @@
 package com.example.bepro;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONObject;
+
 public class LoginActivity extends AppCompatActivity {
-    //로그인 버튼 클릭 시 전환되는 로그인 화면
-    Button Button;
-    EditText EditText;
+    Button login, create, forget;
+    EditText email, pwd;
+    CheckBox autologin, saveid;
+    String userEmail, userPassword, id, password, idSet, pwdSet;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_page);
 
-        EditText = (EditText) findViewById(R.id.loginId);
-        EditText = (EditText) findViewById(R.id.loginPwd);
+        email = (EditText) findViewById(R.id.loginId);
+        pwd = (EditText) findViewById(R.id.loginPwd);
+        login = (Button) findViewById(R.id.login);
+        forget = (Button) findViewById(R.id.findMembtn);
+        create = (Button) findViewById(R.id.createMembtn);
+        autologin = (CheckBox) findViewById(R.id.autologin);
+        saveid = (CheckBox) findViewById(R.id.saveid);
+
+        PrefsHelper.init(getApplicationContext());
+        //PrefsHelper.clear(); // 자동로그인, 아이디 저장 파일 비우기(테스트용)
+
+        //단말 파일 key, value 값 가져오기
+        id = PrefsHelper.read("userEmail", "");
+        password = PrefsHelper.read("userPassword", "");
+        idSet = PrefsHelper.read("saveID", "");
+        pwdSet = PrefsHelper.read("savePWD", "");
+
+        //자동 로그인 (checked)
+        if(id != "" && password != ""){
+            userEmail = id;
+            userPassword = password;
+            Toast.makeText(getApplicationContext(), "자동 로그인되었습니다.", Toast.LENGTH_SHORT).show();
+            Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+            startActivity(intent);
+        }
+
+        //아이디 저장 (checked)
+        if(idSet != "" && pwdSet != ""){
+            email.setText(idSet);
+            pwd.setText(pwdSet);
+        }
 
         loginSetting();
         createAcSetting();
         forgetAcSetting();
     }
 
+    //로그인 버튼
     public void loginSetting(){
-        Button = (Button) findViewById(R.id.login);
-        Button.setOnClickListener(new View.OnClickListener() {
+        login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //회원 정보 확인, 로그인 안내(수정)
-                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
-                startActivity(intent);
+                userEmail = email.getText().toString();
+                userPassword = pwd.getText().toString();
+
+                //자동 로그인 체크가 되면 파일에 put
+                if(autologin.isChecked()){
+                    PrefsHelper.write("userEmail", userEmail);
+                    PrefsHelper.write("userPassword", userPassword);
+                }
+
+                //아이디 비번 저장 체크 확인
+                if(saveid.isChecked()){
+                    PrefsHelper.write("saveID", userEmail);
+                    PrefsHelper.write("savePWD", userPassword);
+                }else if (saveid.isChecked() && PrefsHelper.read("saveID", "") == ""){
+                    PrefsHelper.remove("saveID");
+                    PrefsHelper.remove("savePWD");
+                }
+
+                Response.Listener<String> responseListener = new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        try{
+                            JSONObject jsonObject = new JSONObject(response);
+                            boolean success = jsonObject.getBoolean("success");
+                            if(success){
+                                Toast.makeText(getApplicationContext(), "로그인에 성공했습니다.", Toast.LENGTH_SHORT).show();
+                                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                                startActivity(intent);
+                            }else{
+                                Toast.makeText(getApplicationContext(), "로그인에 실패했습니다..", Toast.LENGTH_SHORT).show();
+                                email.setText(null);
+                                pwd.setText(null);
+                            }
+                        }catch (Exception e){
+                            e.printStackTrace();
+                        }
+                    }
+                };
+                LoginRequest loginRequest = new LoginRequest(userEmail, userPassword, responseListener);
+                RequestQueue queue = Volley.newRequestQueue(LoginActivity.this);
+                queue.add(loginRequest);
             }
         });
     }
 
+    //회원가입 버튼
     public void createAcSetting(){
-        Button = (Button) findViewById(R.id.createMembtn);
-        Button.setOnClickListener(new View.OnClickListener() {
+        create.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 //회원가입
@@ -50,9 +128,9 @@ public class LoginActivity extends AppCompatActivity {
         });
     }
 
+    //아이디 비번 찾기 버튼
     public void forgetAcSetting(){
-        Button = (Button) findViewById(R.id.findMembtn);
-        Button.setOnClickListener(new View.OnClickListener() {
+        forget.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 //회원 정보 찾기
